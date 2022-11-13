dcb0b9dca37f1f3a51a34e64fbf7c0af3b076c61
Mohan Ram T.R <mohanraam2000@gmail.com>
2022-11-13 11:11:50 +0530

    13-01-22

---
+++ 00_Introduction_To_Javascript.js
@@ -0,0 +1,10 @@
+// this is the printing line command in javascript
+console.log("Hello World");
+
+console.log("This program adds three numbers, 1 2 and 3")
+
+function AddNum(a,b,c){
+    return a+b+c
+}
+
+console.log("The sum of three numbers is "+AddNum(1,2,3))
