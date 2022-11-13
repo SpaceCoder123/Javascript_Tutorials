1b78ebbf502727260c64db286e681db1ac92f1b6
Mohan Ram T.R <mohanraam2000@gmail.com>
2022-11-13 16:01:33 +0530

    13-11-22

--- 05_Conditionals.js
+++ 05_Conditionals.js
@@ -1,21 +1,22 @@
-console.log("This file is used to run conditional statements in javascript")
+// console.log("This file is used to run conditional statements in javascript")
 
-// some times we have the need to execute some block of statements based on
-// some condition 
+// // some times we have the need to execute some block of statements based on
+// // some condition 
 
-let age = prompt("Please enter your age")
+// let age = prompt("Please enter your age")
 
-if (age = Number.parseInt(age)){
-    if (age < 18){
-        alert("You cannot vote")
-    }
-    else if (age >= 18){
-        alert("You can vote")
-    }
-}
-else{
-    alert("Please enter an integer")
-}
-
+// if (age = Number.parseInt(age)){
+//     if (age < 18){
+//         alert("You cannot vote")
+//     }
+//     else if (age >= 18){
+//         alert("You can vote")
+//     }
+// }
+// else{
+//     alert("Please enter an integer")
+// }
+age = 21;
+console.log((age<18 ? "You cannot vote":"You can vote")) // this is ternary statement
 
 
