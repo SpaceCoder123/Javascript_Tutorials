c74a2e27ab20debe25b5a62df3a830bc556c6dca
Mohan Ram T.R <mohanraam2000@gmail.com>
2022-11-13 15:53:29 +0530

    13-11-22

---
+++ 05_Conditionals.js
@@ -0,0 +1,21 @@
+console.log("This file is used to run conditional statements in javascript")
+
+// some times we have the need to execute some block of statements based on
+// some condition 
+
+let age = prompt("Please enter your age")
+
+if (age = Number.parseInt(age)){
+    if (age < 18){
+        alert("You cannot vote")
+    }
+    else if (age >= 18){
+        alert("You can vote")
+    }
+}
+else{
+    alert("Please enter an integer")
+}
+
+
+
