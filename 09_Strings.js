a53157f913829daaa9993be12fe85eb141e1ae56
Mohan Ram T.R <mohanraam2000@gmail.com>
2022-11-14 17:51:20 +0530

    14-11-22

---
+++ 09_Strings.js
@@ -0,0 +1,14 @@
+console.log("This program deals with string in JS and their functionalities")
+
+// strings are basically used to store and manipulate text
+
+let string_ex = "Mohan"
+let string_ex_sq = 'Mohan' // line 5 and 6 are considered the same
+console.log(string_ex)
+console.log(string_ex[1]) // returns the character in the string 
+console.log(string_ex.length) // returns the length of the string 
+
+let boy1 = "sam"
+let boy2 = "sameer"
+let sentence = `${boy1} is friend of ${boy2}` // $ behaves as a placeholder
+console.log(sentence)
